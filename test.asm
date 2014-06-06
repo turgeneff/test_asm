c4fc4619ed02c740b724b514b18480ceb0549f2e
turgeneff <turgeneff1@gmail.com>
2014-06-06 18:00:39 +0300

    Test

---
+++ test.asm
@@ -0,0 +1,34 @@
+
+; .386
+
+data segment
+    ; org 100h
+    id1 db 0h
+    id2 db 06h
+    
+    ; id3 dw 0ah
+    
+    ; org 100h
+    id4 dd 0fffh
+    
+    var1 dd 0feabch
+    var2 db 0fh
+    var3 dd 010h
+    var4 db 0h
+    
+data ends
+
+
+code segment
+        ; assume cs:code , ds:data
+start:
+
+    ; mov ax,data
+    ; mov ds,ax
+    
+    
+    ; mov ax,4c00h 
+	; int 21h
+    
+code ends 
+    end start
